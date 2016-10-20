f0717d7882897ac2bcda288befe5c4c421ab196b
Jitendra Sharma <jitendrasharma45565@gmail.com>
2016-10-20 08:08:19 +0530

    Add Password rest

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -18,7 +18,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User", email: "user@example.com", password: "password", password_confirmation: "password" } }
     end
-    assert_equal 0, ActionMailer::Base.deliveries.size
+    assert_equal 1, ActionMailer::Base.deliveries.size
     user = assigns(:user)
     assert_not user.activated?
     # Try to log in before activation.
